27fa3c02f873387226585b53a9ac1436d3e79360
chriswylim <chris-lim@astro.com.my>
2020-06-10 20:44:23 +0800

    Made changes to [XXX}

---
+++ git.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+export VERSION_NUMBER="0.0.2"
+
+git add .
+git commit -m "Made changes to [XXX}"
+# git push origin master
+git tag -a "v$VERSION_NUMBER" -m "New release v$VERSION_NUMBER"
+git push origin master
