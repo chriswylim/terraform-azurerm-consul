a555bad18e8ffb01a62a0d5aef1e63f5e6b26c86
chriswylim <chris-lim@astro.com.my>
2020-06-10 20:49:39 +0800

    Change message: !Hotfix for Terraform 0.12.20

--- git.sh
+++ git.sh
@@ -5,5 +5,5 @@ export COMMIT_MESSAGE="!Hotfix for Terraform 0.12.20"
 git add .
 git commit -m "Change message: $COMMIT_MESSAGE"
 # git push origin master
-git tag -a "v$VERSION_NUMBER" -m "New release v$VERSION_NUMBER"
-git push origin master
+git tag -fa "v$VERSION_NUMBER" -m "New release v$VERSION_NUMBER"
+git push origin --tags
